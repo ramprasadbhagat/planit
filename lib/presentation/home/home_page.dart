e76deb6b3a89047268a7a2bdc1755dd22f18520f
biswanath <biswanath.singh@app.cbnits.com>
2024-03-27 13:59:15 +0530

    responsive design added

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,7 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:planit/presentation/home/home_page_mobile.dart';
-import 'package:planit/presentation/home/home_page_web.dart';
 import 'package:planit/utils/responsive.dart';
 
 @RoutePage()
@@ -15,5 +13,28 @@ class HomePage extends StatelessWidget {
       tablet: HomePageMobile(),
       web: HomePageWeb(),
     );
+  }
+}
+
+class HomePageMobile extends StatelessWidget {
+  const HomePageMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class HomePageWeb extends StatefulWidget {
+  const HomePageWeb({super.key});
+
+  @override
+  State<HomePageWeb> createState() => _HomePageWebState();
+}
+
+class _HomePageWebState extends State<HomePageWeb> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
