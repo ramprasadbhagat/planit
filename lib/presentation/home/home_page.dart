e738ee482a5a515dec9cdee81ae8dccb2f4970a6
biswanath <biswanath.singh@app.cbnits.com>
2024-03-27 15:16:40 +0530

    Router updated

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:planit/utils/responsive.dart';
+import 'package:planit/presentation/home/home_page_mobile.dart';
+import 'package:planit/presentation/home/home_page_web.dart';
 
 @RoutePage()
 class HomePage extends StatelessWidget {
@@ -13,28 +15,5 @@ class HomePage extends StatelessWidget {
       tablet: HomePageMobile(),
       web: HomePageWeb(),
     );
-  }
-}
-
-class HomePageMobile extends StatelessWidget {
-  const HomePageMobile({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
-class HomePageWeb extends StatefulWidget {
-  const HomePageWeb({super.key});
-
-  @override
-  State<HomePageWeb> createState() => _HomePageWebState();
-}
-
-class _HomePageWebState extends State<HomePageWeb> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
