2a416932733effd912dac06d3877466df1d27da6
biswanath <biswanath.singh@app.cbnits.com>
2024-04-06 16:34:31 +0530

    search bar fix

--- lib/presentation/home/shop/widgets/search_bar.dart
+++ lib/presentation/home/shop/widgets/search_bar.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:planit/presentation/theme/colors.dart';
 
 class HomeSearchBar extends StatelessWidget {
   const HomeSearchBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-      child: TextField(
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          hintText: 'Search',
-          prefixIcon: Icon(
-            Icons.search_rounded,
-            size: 24,
-            color: Colors.grey,
-          ),
-          contentPadding: EdgeInsets.symmetric(horizontal: 0),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: AppColors.lightGrey, width: 1.0),
+    const borderDecoration = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(16.0)),
+      borderSide: BorderSide.none,
+    );
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: Material(
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: const TextField(
+          decoration: InputDecoration(
+            focusedBorder: borderDecoration,
+            enabledBorder: borderDecoration,
+            errorBorder: borderDecoration,
+            disabledBorder: borderDecoration,
+            border: borderDecoration,
+            hintText: 'Search',
+            prefixIcon: Icon(
+              Icons.search_rounded,
+              size: 24,
+              color: Colors.grey,
+            ),
+            contentPadding: EdgeInsets.symmetric(horizontal: 0),
           ),
         ),
       ),
