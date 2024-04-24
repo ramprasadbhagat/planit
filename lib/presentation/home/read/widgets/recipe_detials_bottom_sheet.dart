3dcf58b3a2d65bdee329f088ab3e85e260ded2bf
biswanath <biswanath.singh@app.cbnits.com>
2024-04-24 11:01:18 +0530

    Read recipe bottom sheet updated

---
+++ lib/presentation/home/read/widgets/recipe_detials_bottom_sheet.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:planit/domain/home/entities/trending_recipe.dart';
+import 'package:planit/presentation/theme/colors.dart';
+import 'package:planit/utils/png_image.dart';
+import 'package:planit/utils/svg_image.dart';
+
+class RecipeDetailsBottomSheet extends StatelessWidget {
+  final TrendingRecipe trendingRecipe;
+  const RecipeDetailsBottomSheet({super.key, required this.trendingRecipe});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Column(
+      children: [
+        RecipeDetail(
+          trendingRecipe: trendingRecipe,
+        ),
+        const HowToCook(),
+      ],
+    );
+  }
+}
+
+class HowToCook extends StatelessWidget {
+  const HowToCook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text('How to cook'),
+        Text(
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do so eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi utlim aliquip ex ea commodo.',
+        ),
+      ],
+    );
+  }
+}
+
+class RecipeDetail extends StatelessWidget {
+  final TrendingRecipe trendingRecipe;
+  const RecipeDetail({super.key, required this.trendingRecipe});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Stack(
+      children: [
+        SizedBox(
+          height: MediaQuery.sizeOf(context).height * 0.4,
+          child: Image.asset(
+            PngImage.genericDemo('trending_recipe.png'),
+          ),
+        ),
+        Positioned(
+          bottom: 50,
+          left: MediaQuery.sizeOf(context).width * 0.01,
+          child: Card(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              width: MediaQuery.sizeOf(context).width * 0.66,
+              height: MediaQuery.sizeOf(context).height * 0.13,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    trendingRecipe.title,
+                    style: textTheme.labelSmall?.copyWith(
+                      fontSize: 15,
+                    ),
+                  ),
+                  Text(
+                    trendingRecipe.subTitle,
+                    style: textTheme.bodySmall?.copyWith(
+                      fontSize: 12,
+                      color: AppColors.grey1,
+                    ),
+                  ),
+                  const Divider(
+                    height: 20,
+                    thickness: 0.8,
+                    endIndent: 0,
+                    indent: 0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            size: 15,
+                          ),
+                          const SizedBox(
+                            width: 3,
+                          ),
+                          Text(
+                            trendingRecipe.rating.toString(),
+                            style: textTheme.bodyMedium?.copyWith(
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.access_time,
+                            size: 15,
+                            color: AppColors.slateBlue,
+                          ),
+                          const SizedBox(
+                            width: 3,
+                          ),
+                          Text(
+                            '${trendingRecipe.preparationTime.toString()} min',
+                            style: textTheme.bodyMedium?.copyWith(
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SvgPicture.asset(
+                            SvgImage.chefHat,
+                            height: 15,
+                            width: 15,
+                          ),
+                          const SizedBox(
+                            width: 3,
+                          ),
+                          Text(
+                            trendingRecipe.level,
+                            style: textTheme.bodyMedium?.copyWith(
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/presentation/home/read/widgets/trending_recipes.dart
+++ lib/presentation/home/read/widgets/trending_recipes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:planit/domain/home/entities/trending_recipe.dart';
 import 'package:planit/presentation/core/section_title.dart';
+import 'package:planit/presentation/home/read/widgets/recipe_detials_bottom_sheet.dart';
 import 'package:planit/presentation/theme/colors.dart';
 import 'package:planit/utils/png_image.dart';
 import 'package:planit/utils/svg_image.dart';
@@ -50,107 +51,116 @@ class TrendingRecipeCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
 
-    return Stack(
-      children: [
-        Image.asset(
-          PngImage.genericDemo(trendingRecipe.image),
-          height: MediaQuery.sizeOf(context).height * 0.25,
-          width: MediaQuery.sizeOf(context).width * 0.7,
+    return InkWell(
+      onTap: () => showModalBottomSheet<void>(
+        context: context,
+        builder: (BuildContext context) => RecipeDetailsBottomSheet(
+          trendingRecipe: trendingRecipe,
         ),
-        Positioned(
-          bottom: 0.1,
-          left: MediaQuery.sizeOf(context).width * 0.01,
-          child: Card(
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-              width: MediaQuery.sizeOf(context).width * 0.66,
-              height: MediaQuery.sizeOf(context).height * 0.13,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    trendingRecipe.title,
-                    style: textTheme.labelSmall?.copyWith(
-                      fontSize: 15,
+      ),
+      child: Stack(
+        children: [
+          Image.asset(
+            PngImage.genericDemo(trendingRecipe.image),
+            height: MediaQuery.sizeOf(context).height * 0.25,
+            width: MediaQuery.sizeOf(context).width * 0.7,
+          ),
+          Positioned(
+            bottom: 0.1,
+            left: MediaQuery.sizeOf(context).width * 0.01,
+            child: Card(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                width: MediaQuery.sizeOf(context).width * 0.66,
+                height: MediaQuery.sizeOf(context).height * 0.13,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      trendingRecipe.title,
+                      style: textTheme.labelSmall?.copyWith(
+                        fontSize: 15,
+                      ),
                     ),
-                  ),
-                  Text(
-                    trendingRecipe.subTitle,
-                    style: textTheme.bodySmall?.copyWith(
-                      fontSize: 12,
-                      color: AppColors.grey1,
+                    Text(
+                      trendingRecipe.subTitle,
+                      style: textTheme.bodySmall?.copyWith(
+                        fontSize: 12,
+                        color: AppColors.grey1,
+                      ),
                     ),
-                  ),
-                  const Divider(
-                    height: 20,
-                    thickness: 0.8,
-                    endIndent: 0,
-                    indent: 0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(
-                            Icons.star,
-                            size: 15,
-                          ),
-                          const SizedBox(
-                            width: 3,
-                          ),
-                          Text(
-                            trendingRecipe.rating.toString(),
-                            style: textTheme.bodyMedium?.copyWith(
-                              fontSize: 12,
+                    const Divider(
+                      height: 20,
+                      thickness: 0.8,
+                      endIndent: 0,
+                      indent: 0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.star,
+                              size: 15,
                             ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          const Icon(
-                            Icons.access_time,
-                            size: 15,
-                            color: AppColors.slateBlue,
-                          ),
-                          const SizedBox(
-                            width: 3,
-                          ),
-                          Text(
-                            '${trendingRecipe.preparationTime.toString()} min',
-                            style: textTheme.bodyMedium?.copyWith(
-                              fontSize: 12,
+                            const SizedBox(
+                              width: 3,
                             ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          SvgPicture.asset(
-                            SvgImage.chefHat,
-                            height: 15,
-                            width: 15,
-                          ),
-                          const SizedBox(
-                            width: 3,
-                          ),
-                          Text(
-                            trendingRecipe.level,
-                            style: textTheme.bodyMedium?.copyWith(
-                              fontSize: 12,
+                            Text(
+                              trendingRecipe.rating.toString(),
+                              style: textTheme.bodyMedium?.copyWith(
+                                fontSize: 12,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.access_time,
+                              size: 15,
+                              color: AppColors.slateBlue,
+                            ),
+                            const SizedBox(
+                              width: 3,
+                            ),
+                            Text(
+                              '${trendingRecipe.preparationTime.toString()} min',
+                              style: textTheme.bodyMedium?.copyWith(
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              SvgImage.chefHat,
+                              height: 15,
+                              width: 15,
+                            ),
+                            const SizedBox(
+                              width: 3,
+                            ),
+                            Text(
+                              trendingRecipe.level,
+                              style: textTheme.bodyMedium?.copyWith(
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
