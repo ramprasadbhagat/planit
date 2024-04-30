1fe940cb20bfa425fb48e0cf89fd55f1fb4bb80f
biswanath <biswanath.singh@app.cbnits.com>
2024-04-30 12:03:25 +0530

    sub category improvement

--- lib/presentation/category/widgets/sub_category_product_count.dart
+++ lib/presentation/category/widgets/sub_category_product_count.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:planit/application/category/category_bloc.dart';
+import 'package:planit/application/sub_category/sub_category_bloc.dart';
 import 'package:planit/presentation/theme/colors.dart';
 
 class SubCategoryProductCount extends StatelessWidget {
@@ -14,31 +17,39 @@ class SubCategoryProductCount extends StatelessWidget {
         top: 20,
         bottom: 10,
       ),
-      child: RichText(
-        text: TextSpan(
-          children: [
-            TextSpan(
-              text: '57 Items',
-              style: GoogleFonts.montserrat(
-                textStyle: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: AppColors.textBlack,
-                  fontSize: 16, //size.width * 0.04,
+      child: BlocBuilder<SubCategoryBloc, SubCategoryState>(
+        buildWhen: (previous, current) =>
+            previous.selectedSubCategory != current.selectedSubCategory,
+        builder: (context, state) {
+          final products = state.products;
+          if (products.isEmpty) return const SizedBox.shrink();
+          return RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: '${state.products.length} Items',
+                  style: GoogleFonts.montserrat(
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.textBlack,
+                      fontSize: 16, //size.width * 0.04,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            TextSpan(
-              text: ' in Dry Fruits',
-              style: GoogleFonts.montserrat(
-                textStyle: const TextStyle(
-                  fontWeight: FontWeight.w500,
-                  color: AppColors.textBlack,
-                  fontSize: 16, //size.width * 0.04,
+                TextSpan(
+                  text: ' in ${state.selectedSubCategory.name.displayLabel}',
+                  style: GoogleFonts.montserrat(
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.textBlack,
+                      fontSize: 16, //size.width * 0.04,
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
