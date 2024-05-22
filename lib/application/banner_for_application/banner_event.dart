6e24f318cd41df28bfab3c2dfbb166fac6afafc3
cbnits-29 <cbnits-29@cbnits-29s-MacBook-Pro.local>
2024-05-22 12:14:06 +0530

    delete folder

--- lib/application/banner_for_application/banner_event.dart
+++
@@ -1,7 +0,0 @@
-part of 'banner_bloc.dart';
-
-@freezed
-class BannerEvent with _$BannerEvent {
-  const factory BannerEvent.initialized() = _Initialized;
-  const factory BannerEvent.fetch() = _Fetch;
-}
